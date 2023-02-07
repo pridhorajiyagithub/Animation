47cc89a86da7a7350c4edd09da601be7dd0f571b
meera <priyadhorajiya5@gmail.com>
2023-02-07 21:01:56 +0530

    first commit

---
+++ lib/screens/explicit/built in.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class ExplicitBuitInPage extends StatefulWidget {
+  const ExplicitBuitInPage({Key? key}) : super(key: key);
+
+  @override
+  State<ExplicitBuitInPage> createState() => _ExplicitBuitInPageState();
+}
+
+class _ExplicitBuitInPageState extends State<ExplicitBuitInPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2000),
+      // lowerBound: 0,
+      // upperBound: 1
+    );
+    _animation = Tween<Offset>(
+      begin: const Offset(-0.5, 0.0),
+      end: const Offset(0.5, 0.0),
+    ).animate(CurvedAnimation(
+      parent: animationController,
+      curve: Curves.easeInCubic,
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RotationTransition(
+              turns: animationController,
+              child: Image.asset(
+                'assets/img_19509.png',
+                height: 200,
+                width: 200,
+              ),
+            ),
+            SlideTransition(
+              position: _animation,
+              child: Image.asset(
+                'assets/img_19509.png',
+                height: 200,
+                width: 200,
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  animationController.forward();
+                },
+                child: Text("Animate")),
+            ElevatedButton(
+                onPressed: () {
+                  animationController.reverse();
+                },
+                child: Text("Reverse Animate")),
+            ElevatedButton(
+                onPressed: () {
+                  animationController.repeat();
+                },
+                child: Text("Repeat")),
+            ElevatedButton(
+                onPressed: () {
+                  animationController.stop();
+                },
+                child: Text("Stop")),
+            ElevatedButton(
+                onPressed: () {
+                  animationController..repeat(reverse: true);
+                },
+                child: Text("task")),
+          ],
+        ),
+      ),
+    );
+  }
+}
